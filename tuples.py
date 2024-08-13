7c1b43fe371e329f8b8b972ee0fb71a2afbb1ce9
Harsh Ruhil <ruhilharsh8@gmail.com>
2024-08-13 11:29:00 +0530

    Update tuples.py

--- tuples.py
+++ tuples.py
@@ -1,2 +1,6 @@
+#tuple is data structure similar to a list except the fact that it is immutable.
+#this means that there can be no changes made inside a tuple
+#the elements inside a tuple are enclosed within ().
+
 mytuple=(5,8)
 print(mytuple[0])
