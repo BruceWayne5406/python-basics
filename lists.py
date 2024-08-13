8374548b0f2e7422cc761871e61150143368ddf3
Harsh Ruhil <ruhilharsh8@gmail.com>
2024-08-13 10:10:59 +0530

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,11 @@
+mylist=["dad","mom","me","again me"]
+print(mylist)
+
+print(mylist[0])
+print(mylist[-1])
+print(mylist[1:])
+print(mylist[0:2])
+
+print(mylist.index("dad"))
+mylist[3]="again dad"
+print(mylist[3])
