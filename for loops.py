0aa4119340dbacb044acadd4d14f79bcf300ccf1
Harsh Ruhil <ruhilharsh8@gmail.com>
2024-08-13 10:19:49 +0530

    Update for loops.py

--- for loops.py
+++ for loops.py
@@ -2,14 +2,14 @@ for letter in "entrepreneur": #after for we declare a variable like in this case
     print(letter)
 
 
-friends=["larry","elon","bill"]
-for friend in friends:
+friends=["larry","elon","bill"]#created a list here
+for friend in friends:#this means, for each friend in the list "friends" we print the name of each friend,(friend is just a variable)
     print(friend)
 
-for index in range(10):
+for index in range(10):#for "index" in range i.e less than 10,we print numbers from 1 to 10
     print(index)
 
-for index in range(5,9):
+for index in range(5,9):#for each number between 5 to 9 we print the numbers or "indices"
     print(index)
 
 print(2**3) #this is how we use an exponent function,by using to multiplication signs
