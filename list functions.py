65abea81d989e1902ee4bc305752887191482f7f
Harsh Ruhil <ruhilharsh8@gmail.com>
2024-08-13 10:55:29 +0530

    Update list functions.py

--- list functions.py
+++ list functions.py
@@ -1,34 +1,34 @@
-luckynumbers=[5,8,10,15,25]
-friends=["jim","karen","steve","elon"]
+luckynumbers=[5,8,10,15,25]#creation of a list called "lucky numbers". list elements are enclosed inside []
+friends=["jim","karen","steve","elon"]#creation of another list
 
-friends.extend(luckynumbers)
-print(friends)
+friends.extend(luckynumbers)#this joins the list "friends" with "luckynumbers"
+print(friends)#printing the list
 
-friends.append("ambani")
-print(friends)
+friends.append("ambani")#this function appends an element i.e "ambani" in the list "friends"
+print(friends)#printing the list
 
-friends.insert(0,"larry ellison")
-print(friends)
+friends.insert(0,"larry ellison")#this inserts the element "larry ellison" at 
+print(friends)#printing the list
 
-luckynumbers.remove(25)
-print(luckynumbers)
+luckynumbers.remove(25)#this function removes a specific element,in this case 25 from the "luckynumbers" list
+print(luckynumbers)#printing the list
 
-friends.clear()
-print(friends)
+friends.clear()#this function deletes all the elements in the list
+print(friends)#printing the list
 
-luckynumbers.pop()
-print(luckynumbers)
+luckynumbers.pop()#this function deletes an element from the end of the list
+print(luckynumbers)#printing the list
 
 
 
-nlist=["me","me","them","those"]
-print(nlist.count("me"))
+nlist=["me","me","them","those"]#creating another list consisting of string elements
+print(nlist.count("me"))#this function 
 
-luckynumbers.sort()
-print(luckynumbers)
+luckynumbers.sort()#here the numbers are sorted in increasing/decreasing order
+print(luckynumbers)#printing the list
 
-luckynumbers.reverse()
-print(luckynumbers)
+luckynumbers.reverse()#the sequence of all the elements gets reversed
+print(luckynumbers)#printing the list
 
 jnumbers=luckynumbers.copy()
 print(jnumbers)
