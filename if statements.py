e62ba7352119890e73f7e83eec912d0fc4216f96
Harsh Ruhil <ruhilharsh8@gmail.com>
2024-08-13 11:33:13 +0530

    Update if statements.py

--- if statements.py
+++ if statements.py
@@ -1,18 +1,22 @@
+#if conditional statements are used to execute a particular piece of code under a particular condition
+#assigning boolean values to "ismale" and "istall" variables
+#note: boolean values are "True" and "False"
+
 ismale=True
 istall=True
 
-def usecase():
-    if ismale and istall:
+def usecase():#creating a function
+    if ismale and istall:#if this is true , then the following code gets executed
         print("you are male and tall")
     
-    if ismale and not(istall):
+    if ismale and not(istall):#if this is true , then the following code gets executed
         print("you are male but not tall")
 
-    if not(ismale) and istall:
+    if not(ismale) and istall:#if this is true , then the following code gets executed
         print("you are not male but tall")
 
-    elif not(ismale) and not(istall):
+    elif not(ismale) and not(istall):#if this is true , then the following code gets executed
         
         print("you neither a male and nor tall")
     
-usecase()
+usecase()#calling the function
