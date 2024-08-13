2d55c09c51d894ed7ae424dcbd46841bc328e588
Harsh Ruhil <ruhilharsh8@gmail.com>
2024-08-13 10:21:56 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,10 @@
+# function is just a set of algorithms that are executed when the function is called.
+
 m1=input("enter your first num : ")
 m2=input("enter your second num : ")
 
+#to create a function we use the expression "def" and the name of the function that you want to have
 def addnum():
-    print(float(m1)+float(m2))
+    print(float(m1)+float(m2))#instructions inside the function
 
-addnum()
+addnum()#this is how the function is called
